49863e96e777eec25bef6bc2e15faa42b8524940
xidaoW <mwen8103@gmail.com>
2018-09-23 14:49:30 -0400

    add overview

--- src/src/components/Overview/index.js
+++ src/src/components/Overview/index.js
@@ -21,9 +21,9 @@ class OverView extends Component {
 		this.svg;
 		this.layout = {
 			width: 850,
-			height: 300,
+			height: 550,
 			svg: {
-				width: 750, // 90% of whole layout
+				width: 850, // 90% of whole layout
 				height: 550 // 100% of whole layout
 			},
 		};
@@ -55,95 +55,59 @@ class OverView extends Component {
 		this.svg.setAttribute('transform', "translate(" + this.halfRadius * 3 + "," + this.halfRadius * 3 + ")");
 		
 		let data = this.state.dataset;
+		this.pie = d3.pie().sort(null).value(function(d) { return d.size; });
+		this.size = d3.scaleSqrt().domain([0, 1]).range([0, this.halfRadius]);
+
 		data.forEach(function(d) {
 			// petals = max(d.factors.key)
-			d.petals = d3.range(d.dims).map(function(i) { return {size: 1}; }),
-			d.circles = {dominance: d.weight, radius: 10},
-			// d.x = d.tsne_coord.x,
-			// d.y = d.tsne_coord.y;
-			d.x = (d.tsne_coord.x - d.min_tsne[0]) * 650 / (d.max_tsne[0] - d.min_tsne[0]) + 20 ;
-			d.y = (d.tsne_coord.y - d.min_tsne[1]) * 400 / (d.max_tsne[1] - d.min_tsne[1]) + 20;
+			d.petals = d3.range(d.dims).map(function(i) { return {size: 1}; });
+			d.circles = {dominance: d.weight, radius: 10};			
+			d.x = (d.tsne_coord.x - d.min_tsne[0]) * 650 / (d.max_tsne[0] - d.min_tsne[0]) + 100 ;
+			d.y = (d.tsne_coord.y - d.min_tsne[1]) * 400 / (d.max_tsne[1] - d.min_tsne[1]) + 100;
 		});
 
 		console.log(data);
-		this.pie = d3.pie().sort(null).value(function(d) { return d.size; });
-		this.size = d3.scaleSqrt().domain([0, 1]).range([0, this.halfRadius]);
 
 		const background = d3.select(this.svg)
 						.append('g')	
 						.attr("class", "background")
-						// .attr("transform", function(d, i) { 
-						// 	 return "translate(" + d.x + "," + d.y + ")"; 
-						// });
-		const flower = background.append('g')
-								.attr('class', 'flower')
-		const petal = flower.append('g')
-								.attr('class', 'petal')
 
 		const flowers = background.selectAll('.flower')
 								.data(data)
-								.enter().append('circle')
-								.attr("class", "node")
-								.attr("r", function(d) { return 10; })
-								.attr("fill", "#f46d43")
-								.attr("opacity", function(d) { return d.dominance; })
-								.attr("stroke", "black")
+								.enter().append('g')
 								.attr("transform", function(d, i) { 
 								    return "translate(" + d.x + "," + d.y + ")"; 
 								  });
-		  
 		const petals = flowers.selectAll(".petal")
-		  .data((d) => this.pie(d.petals))
-		.enter().append("path")
-		  .attr("class", "petal")
-		  .attr("transform", function(d) { return r((d.startAngle + d.endAngle) / 2); })
-		  .attr("d", (d) => petalPath(d, this.halfRadius))
-		  .style("stroke", petalStroke)
-		  .style("fill", petalFill);
-
-		// const circle = d3.select(flower).selectAll(".circle")
-		//   .data(function(d) { return pie(d.circles); })
-		//   .enter().append("circle")
-		//   .attr("class", "node")
-		//   .attr("r", function(d) { return d.data.radius; })
-		//   .attr("fill", "#f46d43")
-		//   .attr("opacity", function(d) { return d.data.dominance; })
-		//   .attr("stroke", "black");
-		//   // .attr("transform", function(d, i) { 
-		//   //     return "translate(" + d.x + "," + d.y + ")"; 
-		//   //   });
+							.data((d) => this.pie(d.petals))
+							.enter().append("path")
+							.attr("class", "petal")
+							.attr("transform", (d) => r((d.startAngle + d.endAngle) / 2))
+							.attr("d", (d) => petalPath(d, this.halfRadius))
+							.style("stroke", (d, i) => petalStroke(d, i))
+							.style("fill", (d, i) => petalFill(d, i, this.petals));
 
 
-		// setInterval(update, 400000)
+		const circles = flowers.selectAll('.circle')
+								.data(data)
+								.enter().append('circle')
+								.attr("class", "node")
+								.attr("r", function(d) { return d.circles.radius; })
+								.attr("fill", "#f46d43")
+								.attr("opacity", function(d) { return d.weight; })
+								.attr("stroke", "black")
 
-		// function update() {
-		//   data.forEach(function(flower) {
-		//     flower.petals.forEach(function(d) { d.size = Math.random(); });
-		//   })
-		//   data.sort(function(a, b) {
-		//     return d3.descending(flowerSum(a), flowerSum(b));
-		//   });
 
-		//   flower.data(grid(data), function(d) { return d.id; }).transition().delay(function(d, i) { return 1000 + i * 20; }).duration(1000)
-		//     .attr("transform", function(d, i) { 
-		//       return "translate(" + d.x + "," + d.y + ")"; 
-		//     });
-		  
-		//   petal.data(function(d) { return pie(d.petals); }).transition().duration(1000)
-		//     .attr("transform", function(d) { return r((d.startAngle + d.endAngle) / 2); })
-		//     .attr("d", petalPath);
-		// }
+		function petalPath(d, halfRadius) {
+		  var size = d3.scaleSqrt().domain([0, 1]).range([0, halfRadius]);
 
-		function petalPath(d, halfRadius, this) {
 		  var angle = (d.endAngle - d.startAngle) / 2,
 			  s = polarToCartesian(-angle, halfRadius),
 			  e = polarToCartesian(angle, halfRadius),
-			  r = this.size(1),     
+			  r = size(10),     
 			  m = petalRadius(r, halfRadius),
 			  c1 = {x: halfRadius + r / 2, y: s.y},
 			  c2 = {x: halfRadius + r / 2 , y: e.y};
-			  console.log(angle)
-			  console.log(s)
 		  return "M0,0L" + s.x + "," + s.y + "Q" + c1.x + "," + c1.y + " " + m.x + "," + m.y + "L" +  m.x + "," + m.y +
 		   "Q" + c2.x + "," + c2.y + " " + e.x + "," + e.y + "Z";
 
@@ -173,12 +137,12 @@ class OverView extends Component {
 		  };
 		};
 
-		function petalFill(d, i) {
-		  return d3.hcl(i / this.petals * 360, 60, 70);
+		function petalFill(d, i, petals) {
+		  return d3.hcl(i / petals * 360, 60, 70);
 		};
 
-		function petalStroke(d, i) {
-		  return d3.hcl(i / this.petals * 360, 60, 40);
+		function petalStroke(d, i,petals) {
+		  return d3.hcl(i / petals * 360, 60, 40);
 		};
 
 	  return (
