3e9bb3b99aae34ae47d8ab3c51874757103f0607
xidaoW <mwen8103@gmail.com>
2018-09-23 14:49:30 -0400

    add overview

--- src/src/components/App/index.js
+++ src/src/components/App/index.js
@@ -9,10 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React. This is a test</h1>
+          <h1 className="App-title">Tensor Pattern Exploration</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Overview />
       </div>
