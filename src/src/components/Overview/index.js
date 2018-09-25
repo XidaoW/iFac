3edc709275e4e3e7349c499a9ff384de760ef2ad
ayong8 <yong8@me.com>
2018-09-25 15:46:25 -0400

    Restructure the data flow; Make all the data come from the top component as state

--- src/src/components/Overview/index.js
+++ src/src/components/Overview/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 import Grid from 'd3-v4-grid';
-import factors_data from '../../data/factors.json'
 import ReactFauxDOM from 'react-faux-dom';
 
 import _ from 'lodash';
@@ -9,13 +8,10 @@ import styles from './styles.scss';
 import index from '../../index.css';
 // import gs from '../../config/_variables.scss'; // gs (=global style)
 
-/* props: this.props.ranking
-  => selected ranking data
-*/
 class OverView extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {dataset: factors_data};
+
 		this.pie;
 		this.size;
 		this.svg;
@@ -27,40 +23,40 @@ class OverView extends Component {
 				height: 550 // 100% of whole layout
 			},
 		};
+
 		this.petals = 3;
 		this.halfRadius = 15;
 		this.circleRadius = 10;
 	}
 
-
 	render() {
-		let self = this;
+		if (!this.props.data || this.props.data.length === 0)
+			return <div />
+
+		const _self = this;
+		const { data } = this.props;
+
 		this.svg = new ReactFauxDOM.Element('svg');
 		this.svg.setAttribute('width', this.layout.svg.width);
 		this.svg.setAttribute('height', this.layout.svg.height);
 		this.svg.setAttribute('transform', "translate(" + this.halfRadius * 3 + "," + this.halfRadius * 3 + ")");
 		
-		let data = this.state.dataset;
 		this.pie = d3.pie().sort(null).value(function(d) { return d.size; });
-		this.size = d3.scaleSqrt().domain([0, 1]).range([0, this.halfRadius]);
-
-		data.forEach(function(d) {
-			d.petals = d3.range(d.dims).map(function(i) { return {size: 1}; });
-			d.circles = {dominance: d.weight, radius: 10};			
-			d.x = (d.tsne_coord.x - d.min_tsne[0]) * 650 / (d.max_tsne[0] - d.min_tsne[0]) + 100 ;
-			d.y = (d.tsne_coord.y - d.min_tsne[1]) * 400 / (d.max_tsne[1] - d.min_tsne[1]) + 100;
-		});
+		this.size = d3.scaleSqrt()
+					.domain([0, 1])
+					.range([0, this.halfRadius]);
 
 		const background = d3.select(this.svg)
 						.append('g')	
-						.attr("class", "background")
+						.attr("class", "background");
 
 		const flowers = background.selectAll('.flower')
 								.data(data)
 								.enter().append('g')
 								.attr("transform", function(d, i) { 
 								    return "translate(" + d.x + "," + d.y + ")"; 
-								  });
+									});
+									
 		const petals = flowers.selectAll(".petal")
 							.data((d) => this.pie(d.petals))
 							.enter().append("path")
@@ -69,7 +65,6 @@ class OverView extends Component {
 							.attr("d", (d) => petalPath(d, this.halfRadius))
 							.style("stroke", (d, i) => petalStroke(d, i))
 							.style("fill", (d, i) => petalFill(d, i, this.petals));
-
 
 		const circles = background.selectAll('.circle')
 								.data(data)
@@ -82,7 +77,6 @@ class OverView extends Component {
 								.attr("transform", function(d, i) { 
 								    return "translate(" + d.x + "," + d.y + ")"; 
 								  });
-
 
 		function petalPath(d, halfRadius) {
 		  var size = d3.scaleSqrt().domain([0, 1]).range([0, halfRadius]);
@@ -113,7 +107,6 @@ class OverView extends Component {
 		}
 
 		function polarToCartesian(angle, radius) {
-		  
 		  return {
 			// start and end of the petal
 			// size of the petal
@@ -132,10 +125,10 @@ class OverView extends Component {
 		};
 
 	  return (
-		<div className={styles.PatternOverview}>
-		  <div className={index.title}>Overview</div>
-		  {this.svg.toReact()}
-		</div>
+			<div className={styles.PatternOverview}>
+				<div className={index.title}>Overview</div>
+				{this.svg.toReact()}
+			</div>
 	  );
 
 	}
--- src/src/components/PatternDetailView/index.js
+++ src/src/components/PatternDetailView/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
-import factors_data from '../../data/factors.json'
-import bar_data from '../../data/data.csv'
+import factors_data from '../../data/factors.json';
 import ReactFauxDOM from 'react-faux-dom';
 
 import styles from './styles.scss';
@@ -14,10 +13,7 @@ import index from '../../index.css';
 class PatternDetailView extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			dataset: []
-		};
-		// svg;
+
 		this.layout = {
 			width: 850,
 			height: 550,
@@ -28,49 +24,26 @@ class PatternDetailView extends Component {
 		};
 	}
 
-	componentDidMount() {
-		const _self = this;
-		d3.csv(bar_data).then(function(data){
-			_self.setState({
-				dataset: data
-			});
-		});
-	}
-
 	render() {
-		if (!this.state.dataset || this.state.dataset.length === 0)
-			return <div />
+		const { data } = this.props;
 
-		const data = this.state.dataset;
-		console.log(data);
-		var svg = new ReactFauxDOM.Element('svg');
+		const svg = new ReactFauxDOM.Element('svg');
 		svg.setAttribute('width', this.layout.svg.width);
 		svg.setAttribute('height', this.layout.svg.height);
-		// let data = this.state.dataset;
 
-		var margin = {top: 20, right: 20, bottom: 30, left: 40},
-		    width = +this.layout.svg.width - margin.left - margin.right,
-		    height = +this.layout.svg.height - margin.top - margin.bottom;
+		const margin = {top: 20, right: 20, bottom: 30, left: 40},
+          width = +this.layout.svg.width - margin.left - margin.right,
+          height = +this.layout.svg.height - margin.top - margin.bottom;
 
-
-		data.forEach(function(d, columns) {
-			for (var i = 1, n = columns.length; i < n; ++i) 
-				d[columns[i]] = +d[columns[i]];		
-			return d
-		});
-
-		var x0 = d3.scaleBand()
-		    .rangeRound([0, width])
-		    .paddingInner(0.1),
-
-			x1 = d3.scaleBand()
-			    .padding(0.05),
-
-			y = d3.scaleLinear()
-			    .rangeRound([height, 0]),
-
-			z = d3.scaleOrdinal()
-			    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+		const x0 = d3.scaleBand()
+            .rangeRound([0, width])
+            .paddingInner(0.1),
+          x1 = d3.scaleBand()
+            .padding(0.05),
+          y = d3.scaleLinear()
+            .rangeRound([height, 0]),
+          z = d3.scaleOrdinal()
+            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
   
 		  var keys = data.columns.slice(1);
 
@@ -78,35 +51,29 @@ class PatternDetailView extends Component {
 		  x1.domain(keys).rangeRound([0, x0.bandwidth()]);
 		  y.domain([0, d3.max(data, function(d) { return d3.max(keys, function(key) { return d[key]; }); })]).nice();
 
-		  console.log(data);
-
 		  const g = d3.select(svg).append("g")
 					.attr("class", "detailView")
 					.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 		  const rect_plot = g.selectAll(".detailView_col")
-		    .data(data)
-		    .enter().append("g")
-		    .attr("class","detailView_col")
-		    .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
-		      
-
-		  console.log(rect_plot);
+          .data(data)
+          .enter().append("g")
+          .attr("class","detailView_col")
+          .attr("transform", function(d) { return "translate(" + x0(d.State) + ",0)"; });
 
 		  rect_plot.selectAll(".rect")
-		    .data(function(d) { 
-		    	return keys.map(function(key) { 
-		    		console.log({key: key, value: d[key]}); 
-		    		return {key: key, value: d[key]}; 
-		    	}); 
-			})
-		    .enter().append("rect")
-		    .attr("class", "rect")
-		      .attr("x", function(d) { return x1(d.key); })
-		      .attr("y", function(d) { return y(d.value); })
-		      .attr("width", x1.bandwidth())
-		      .attr("height", function(d) { return height - y(d.value); })
-		      .attr("fill", function(d) { return z(d.key); });
+          .data(function(d) { 
+            return keys.map(function(key) { 
+              return {key: key, value: d[key]}; 
+            }); 
+          })
+          .enter().append("rect")
+          .attr("class", "rect")
+          .attr("x", function(d) { return x1(d.key); })
+          .attr("y", function(d) { return y(d.value); })
+          .attr("width", x1.bandwidth())
+          .attr("height", function(d) { return height - y(d.value); })
+          .attr("fill", function(d) { return z(d.key); });
 
 		  g.append("g")
 		      .attr("class", "axis")
@@ -116,7 +83,7 @@ class PatternDetailView extends Component {
 		  g.append("g")
 		      .attr("class", "axis")
 		      .call(d3.axisLeft(y).ticks(null, "s"))
-		    .append("text")
+		      .append("text")
 		      .attr("x", 2)
 		      .attr("y", y(y.ticks().pop()) + 0.5)
 		      .attr("dy", "0.32em")
@@ -129,10 +96,10 @@ class PatternDetailView extends Component {
 		      .attr("font-family", "sans-serif")
 		      .attr("font-size", 10)
 		      .attr("text-anchor", "end")
-		    .selectAll("g")
-		    .data(keys.slice().reverse())
-		    .enter().append("g")
-		      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+          .selectAll("g")
+          .data(keys.slice().reverse())
+          .enter().append("g")
+          .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
 		  legend.append("rect")
 		      .attr("x", width - 19)
@@ -147,10 +114,10 @@ class PatternDetailView extends Component {
 		      .text(function(d) { return d; });
 
 	  return (
-		<div className={styles.PatternOverview}>
-		  <div className={index.title}>Pattern Detail View</div>
-		  {svg.toReact()}
-		</div>
+      <div className={styles.PatternOverview}>
+        <div className={index.title}>Pattern Detail View</div>
+        {svg.toReact()}
+      </div>
 	  );
 
 	}
