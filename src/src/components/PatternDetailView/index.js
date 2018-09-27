7ab536425b3bcea77fa3e98923e76fcf3049224c
ayong8 <yong8@me.com>
2018-09-27 11:30:00 -0400

    Review the code part of bar adding and removing with click interaction

--- src/src/components/PatternDetailView/index.js
+++ src/src/components/PatternDetailView/index.js
@@ -26,33 +26,35 @@ class PatternDetailView extends Component {
 	render() {
 		// console.log(this.props.selectedPatterns);
 		const { data, selectedPatterns } = this.props;
-		const svg = new ReactFauxDOM.Element('svg');
+		const svg = new ReactFauxDOM.Element('svg'),
+					descriptor_size = Object.keys(data).length;
+		let g;
+
 		svg.setAttribute('width', this.layout.svg.width);
 		svg.setAttribute('height', this.layout.svg.height);
 		
 		const margin = {top: 10, right: 20, bottom: 200, left: 40},
-	          width = +this.layout.svg.width - margin.left - margin.right,
-    	      height = +this.layout.svg.height - margin.top - margin.bottom;    
-
-		let descriptor_size = Object.keys(data).length;
-		let g;
+	        width = +this.layout.svg.width - margin.left - margin.right,
+    	    height = +this.layout.svg.height - margin.top - margin.bottom;
 		
 		// draw the axis for each descriptor
 		for(var i = 0; i < descriptor_size; i++){
-			svg, g = draw_axis(svg, g, i, width, height, descriptor_size, data)		
-			if(selectedPatterns.length > 0){
-				draw_bars(svg, g, data, i, selectedPatterns, descriptor_size, margin, width, height)
+			draw_axis(i, width, height, descriptor_size, data);
+			if (selectedPatterns.length > 0) {
+				draw_bars(data, i, selectedPatterns, descriptor_size, margin, width, height);
 			}
-
 		}
 
-		function draw_axis(svg, g, i, width, height, descriptor_size, data){
-			height = height/descriptor_size
-			let x0 = d3.scaleBand().rangeRound([0, width]).paddingInner(0.1);
+		function draw_axis(i, width, height, descriptor_size, data){
 			let items;
-			items = Object.keys(data[i][0]).sort();
-			items.pop("id");
-			x0.domain(items);
+			items = Object.keys(data[i][0]).filter((d) => d !== "id").sort();
+			height = height / descriptor_size;
+
+			const x0 = d3.scaleBand()
+							.domain(items)
+							.rangeRound([0, width])
+							.paddingInner(0.1);
+
 			g = d3.select(svg).append("g")
 				.attr("class", "detailView")
 				.attr("id", "descriptor"+i)
@@ -66,31 +68,37 @@ class PatternDetailView extends Component {
 				.style("text-anchor", "end")
 				.attr("dx", "-.8em")
 				.attr("dy", ".01em")      
-				.attr("transform", function(d) {
-					return "rotate(-65)" 
-				});
+				.attr("transform", (d) => "rotate(-65)");
+
 			return svg, g	
 		}
 
-    	function draw_bars(svg, g, data, i, selectedPatterns, descriptor_size, margin, width, height){
-			height = height / descriptor_size
-			let x0 = d3.scaleBand().rangeRound([0, width]).paddingInner(0.1),
-				x1 = d3.scaleBand().padding(0.05),
-				y = d3.scaleLinear().rangeRound([height, 0]),
-				z = d3.scaleOrdinal().range(d3.schemePaired);
-				// z = d3.scaleOrdinal().range(['#8da0cb','#e78ac3','#a6d854']);
+    function draw_bars(data, i, patternIndices, descriptor_size, margin, width, height) {
+			let patterns, items;
+			patterns = patternIndices.map((pattern_id) => data[i][pattern_id]);
+			items = Object.keys(data[i][0]).filter((d) => d !== "id").sort();
+			
+			height = height / descriptor_size;
+			const x0 = d3.scaleBand()
+							.domain(items)
+							.rangeRound([0, width])
+							.paddingInner(0.1),
+						x1 = d3.scaleBand()
+							.domain(patternIndices)
+							.rangeRound([0, x0.bandwidth()])
+							.padding(0.05),
+						y = d3.scaleLinear()
+							.rangeRound([height, 0]),
+						z = d3.scaleOrdinal().range(d3.schemePaired);
+						// z = d3.scaleOrdinal().range(['#8da0cb','#e78ac3','#a6d854']);
 
-			let data_, items;
-			items = Object.keys(data[i][0]).sort();
-			items.pop("id");
-			x0.domain(items);
+			y.domain([0, d3.max(patterns, (d) =>
+					d3.max(items, (key) => d[key])) ]
+				).nice();
 
-			data_ = selectedPatterns.map(function(pattern_id) { return data[i][pattern_id]; });    		
-			x1.domain(selectedPatterns).rangeRound([0, x0.bandwidth()]);
-			y.domain([0, d3.max(data_, function(d) { console.log(d3.max(items, function(key) { return d[key]; })); return d3.max(items, function(key) { return d[key]; }); })]).nice();
 			g.selectAll(".detailView_col")
-				.select("#descriptor"+i)
-				.data(data_)
+				.select("#descriptor" + i)
+				.data(patterns)
 				.enter().append("g")
 				.attr("class","detailView_col")
 				.attr("transform", function(d) { 
@@ -109,47 +117,7 @@ class PatternDetailView extends Component {
 				.attr("width", x1.bandwidth())
 				.attr("height", function(d) { return height - y(d.value); })
 				.attr("fill", function(d) { return z(d.id); });
-			// return svg, g
 		}
-				
-
-
-
-			// g.append("g")
-			// 	.attr("class", "axis")
-			// 	.call(d3.axisLeft(y).ticks(null, "s"))
-			// 	.append("text")
-			// 	.attr("x", 2)
-			// 	.attr("y", y(y.ticks().pop()) + 0.5)
-			// 	.attr("dy", "0.32em")
-			// 	.attr("fill", "#000")
-			// 	.attr("font-weight", "bold")
-			// 	.attr("text-anchor", "start")
-			// 	.text("");
-
-			// legend = g.append("g")
-			// 	.attr("font-family", "sans-serif")
-			// 	.attr("font-size", 10)
-			// 	.attr("text-anchor", "end")
-			// 	.selectAll("g")
-			// 	.data(pattern_ids.slice().reverse())
-			// 	.enter().append("g")
-			// 	.attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-			// legend.append("rect")
-			// 	.attr("x", width - 19)
-			// 	.attr("width", 19)
-			// 	.attr("height", 19)
-			// 	.attr("fill", z);
-
-			// legend.append("text")
-			// 	.attr("x", width - 24)
-			// 	.attr("y", 9.5)
-			// 	.attr("dy", "0.32em")
-			// 	.text(function(d) { return "Pattern " + d; });	    	      		
-
-    
-
 
 	  return (
       <div className={styles.PatternOverview}>
