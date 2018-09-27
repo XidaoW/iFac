bec968188b941692db84533350076b65e9e2cfc6
xidaoW <mwen8103@gmail.com>
2018-09-26 20:02:15 -0400

    bind interaction

--- src/src/components/App/index.js
+++ src/src/components/App/index.js
@@ -13,30 +13,53 @@ class App extends Component {
 
 		this.state = {
       factors_data: factors_data,
-      bar_data: []
+      bar_data: [],
+      selectedPattern: []
 		};
+    this.handleClickPattern = this.handleClickPattern.bind(this);
+    this.handleUnClickPattern = this.handleUnClickPattern.bind(this);
 	}
+
+
+  handleClickPattern(id) {    
+    
+    if (!typeof(this.state.selectedPattern) === "number"){      
+      this.setState({
+        selectedPattern: this.state.selectedPattern.concat([id])
+      });      
+    }else{
+      this.setState({
+        selectedPattern: [this.state.selectedPattern].concat([id])
+      });
+    }
+  }
+  handleUnClickPattern(id) {
+    this.setState({
+      selectedPattern: this.state.selectedPattern.pop(id)
+    });
+  }
 
   // Being called before rendering (preparing data to pass it to children)
 	componentWillMount() {
     const _self = this;
 		const factors = factors_data;
 
-    // Update factors data
 		factors.forEach(function(d) {
       d.petals = d3.range(d.dims).map(function(i) { 
         return {length: d.factors[i].entropy,
             width: d.factors[i].similarity.average}; 
       });
 			d.circles = {dominance: d.weight, radius: 10};			
-			d.x = (d.tsne_coord.x - d.min_tsne[0]) * 650 / (d.max_tsne[0] - d.min_tsne[0]) + 100;
-			d.y = (d.tsne_coord.y - d.min_tsne[1]) * 400 / (d.max_tsne[1] - d.min_tsne[1]) + 100;
+			d.x = (d.tsne_coord.x - d.min_tsne[0]) * 450 / (d.max_tsne[0] - d.min_tsne[0]) + 100;
+			d.y = (d.tsne_coord.y - d.min_tsne[1]) * 300 / (d.max_tsne[1] - d.min_tsne[1]) + 100;
     });
 
     var bar_data = {};
     for(var i = 0; i < factors_data[0].dims; i++){
       bar_data[i] = []
-      for(var j = 0; j < factors_data.length; j++) {
+      var pattern_cnt = factors_data.length;
+      // var pattern_cnt = 3;
+      for(var j = 0; j < pattern_cnt; j++) {
         bar_data[i].push(factors_data[j].factors[i].values); 
       }      
     }
@@ -60,8 +83,14 @@ class App extends Component {
           <h1 className="App-title">Tensor Pattern Exploration</h1>
         </header>
         <div className = {styles.wrapper}>
-            <Overview data={factors_data}/>
-            <PatternDetailView data={bar_data}/>
+            <Overview data={factors_data}
+                      onClickPattern={this.handleClickPattern}
+                      onUnClickPattern={this.handleUnClickPattern}
+                      selectedPattern={this.state.selectedPattern}
+                      />
+            <PatternDetailView data={bar_data}                                 
+                                selectedPattern={this.state.selectedPattern}
+                                />
         </div>
         
       </div>
