dd4990f2096a0b88b986e2cf0b1672864c2df969
xidaoW <mwen8103@gmail.com>
2018-11-14 11:45:58 -0500

    update

--- src/src/components/ControlView/index.js
+++ src/src/components/ControlView/index.js
@@ -15,11 +15,11 @@ class ControlView extends Component {
 		this.pie;
 		this.svg;
 		this.layout = {
-			width: 650,
-			height: 550,
+			width: 250,
+			height: 150,
 			svg: {
-				width: 650, // 90% of whole layout
-				height: 550 // 100% of whole layout
+				width: 250, // 90% of whole layout
+				height: 150 // 100% of whole layout
 			},
 		};
 		this.petals = 3;
@@ -28,190 +28,78 @@ class ControlView extends Component {
 	}
 
 	render() {
-		if (!this.props.data || this.props.data.length === 0)
-			return <div />
+		// if (!this.props.scree || this.props.scree.length === 0)
+		return <div />
 
+		console.log(this.props);
 		const _self = this;
-		const { data, selectedPatterns,mostSimilarPatternToSelectedPatternIdx,leastSimilarPatternToSelectedPatternIdx } = this.props;
-		this.petals = data[0].dims;
-		console.log(leastSimilarPatternToSelectedPatternIdx);
+		const { screeData } = this.props;
 		this.svg = new ReactFauxDOM.Element('svg');
 		this.svg.setAttribute('width', this.layout.svg.width);
 		this.svg.setAttribute('height', this.layout.svg.height);
-		this.svg.setAttribute('transform', "translate(" + this.outerCircleRadius * 3 + "," + this.outerCircleRadius * 3 + ")");		
 		this.pie = d3.pie().sort(null).value(function(d) { return 1; });
-		
-
-		// remove used colors for patterns
-		var color_list = ["#ffff99", "#fdc086", "#beaed4"];
-		for(var i = 0; i < selectedPatterns.length; i++){
-			var used_color = d3.select("#pattern_" + selectedPatterns[i]).attr("stroke");		
-			color_list.splice( color_list.indexOf(used_color), 1 );
+		console.log(screeData);
+		var n = screeData.length;
+		var y_values = [];
+		for(var i = 0; i < n-1; i++){
+			y_values.push(screeData[i]['y']);
 		}
-		// PLOT THE BACKDROP
-		const backdrop = d3.select(this.svg)
-						.append('g')
-						.attr("class", "background");
+		
+		console.log(y_values);
+		var xScale = d3.scaleLinear()
+		    .domain([0, n-1]) // input
+		    .range([0, this.layout.svg.width]); // output
 
-		backdrop.selectAll('.button')
-				.append('g')
-				.attr('class', 'button')
-				.attr("text", "TEST");
+		// 6. Y scale will use the randomly generate number 
+		var yScale = d3.scaleLinear()
+		    .domain([0, Math.max(y_values)]) // input 
+		    .range([this.layout.svg.height, 0]); // output 
 
-		// ADD TOOLTIP
-		const div_tooltip = d3.select("body").append("div")
-								.attr("id", "tooltip")
-								.attr("class", "tooltip")
-								.style("opacity", 0);		
+		// 7. d3's line generator
+		var line = d3.line()
+		    .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
+		    .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
+		    // .curve(d3.curveMonotoneX) // apply smoothing to the line
 
-		// PLOT THE FLOWERS ==> PATTERNS
-		const flowers = backdrop.selectAll('.flower')
-								.data(data)
-								.enter().append('g')
-								.attr("class", "flower")
-								.attr("id", (d) => "flower_"+d.id)
-								.attr("transform", function(d, i) { 
-								    return "translate(" + d.x + "," + d.y + ")"; 
-									});
-		// ADD THE PETALS TO FLOWERS ==> DESCRIPTORS								
-		const petals = flowers.selectAll(".petal")
-							.data((d) => this.pie(d.petals))
-							.enter().append("path")
-							.attr("class", "petal")
-							// .attr("class", "petal")
-							.attr("transform", (d) => petal.rotateAngle((d.startAngle + d.endAngle) / 2))
-							.attr("d", (d) => petal.petalPath(d, this.outerCircleRadius))
-							.style("stroke", (d, i) => 'gray')
-							.on("mouseover", function(d) {
-								div_tooltip.transition()
-									.duration(200)
-									.style("opacity", .9);
+		var dataset = screeData;
+		// 3. Call the x axis in a group tag
+		d3.select(this.svg).append("g")
+		    .attr("class", "x axis")
+		    .attr("transform", "translate(0," + this.layout.svg.height + ")")
+		    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
 
-								var t = d3.select(this.parentNode).attr("transform");
-								var translate = t.substring(t.indexOf("(")+1, t.indexOf(")")).split(",");
+		// 4. Call the y axis in a group tag
+		d3.select(this.svg).append("g")
+		    .attr("class", "y axis")
+		    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
 
-								div_tooltip
-									.html("Entropy: " + d.data.length + "</br>" + "Similarity: " + d.data.width)
-									.style("left", translate[0] + "px")
-									.style("top", translate[1] + "px");
-						   })
-							.on("mouseout", function(d) {
-								div_tooltip.transition()
-									.duration(500)
-									.style("opacity", 0);
-							})							
-							.style("fill", (d, i) => petal.petalFill(d, i, this.petals))
-							.style('fill-opacity', 0.8);
+		// 9. Append the path, bind the data, and call the line generator 
+		d3.select(this.svg).append("path")
+		    .datum(dataset) // 10. Binds data to the line 
+		    .attr("class", "line") // Assign a class for styling 
+		    .attr("d", line); // 11. Calls the line generator 
 
-
-		// console.log(mostSimilarPatternToSelectedPatternIdx);
-	 //    for(var p_idx = 0; p_idx < mostSimilarPatternToSelectedPatternIdx.length; i++){
-	 //    	petals.style("stroke", function(d,i,p_idx){
-	 //    		if(i == p_idx && d.data.id == mostSimilarPatternToSelectedPatternIdx[p_idx]){
-	 //    			return "black";
-	 //    		}
-
-	 //    	});
-	 //    }
-
-		// ADD THE OUTER CIRCLES TO THE BACKDROP								
-		const circles1 = backdrop.selectAll('.circle')
-								.data(data)
-								.enter().append('circle')
-								.attr("class", "outer_circle")
-								.attr("r", this.outerCircleRadius)
-								.attr("fill", "white")
-								.attr("stroke-width", gs.outerCircleStrokeWidth)
-								.attr("stroke-opacity", gs.outerCircleStrokeOpacity)
-								.attr("fill-opacity", 0)
-								.attr("id", function(d) { return "pattern_" + d.id; })								
-								.attr("transform", function(d, i) { 
-									return "translate(" + d.x + "," + d.y + ")"; 
-								})
-								.on("click", (d) => {
-									if (d3.select("#pattern_" + d.id).classed("selected")) {
-										_self.props.onUnClickPattern(d.id);
-										var cancel_color = d3.select("#pattern_" + d.id).attr("stroke");
-										d3.select("#pattern_" + d.id).classed("selected", false);																				
-										d3.select("#pattern_" + d.id).attr("stroke", "none");
-									} else {
-										if(selectedPatterns.length < this.compare_N){
-											_self.props.onClickPattern(d.id);
-											d3.select("#pattern_" + d.id).classed("selected", true);
-											d3.select("#pattern_" + d.id).attr("stroke", color_list[0]);										
-											color_list.splice(0, 1);
-										}
-									}
-								});
-
-
-		// ADD THE INNER CIRCLES TO THE BACKDROP
-		const circles = backdrop.selectAll('.circle')
-								.data(data)
-								.enter().append('circle')
-								.attr("class", "inner_circle")
-								.attr("r", gs.innerCircleRadius)
-								.attr("fill", "#fc8d62")
-								.attr("stroke-width", gs.innerCircleStrokeWidth)								
-								.attr("fill-opacity", function(d) { return d.weight; })													
-								.attr("stroke-opacity", gs.innerCircleStrokeOpacity)																													
-								.attr("transform", function(d, i) { 
-								    return "translate(" + d.x + "," + d.y + ")"; 
-								  })
-								.on("click", (d) => {
-									if (d3.select("#pattern_" + d.id).classed("selected")) {
-										_self.props.onUnClickPattern(d.id);
-										var cancel_color = d3.select("#pattern_" + d.id).attr("stroke");
-										d3.select("#pattern_" + d.id).classed("selected", false);																				
-										d3.select("#pattern_" + d.id).attr("stroke", "none");
-									} else {
-										if(selectedPatterns.length < this.compare_N){
-											_self.props.onClickPattern(d.id);
-											d3.select("#pattern_" + d.id).classed("selected", true);
-											d3.select("#pattern_" + d.id).attr("stroke", color_list[0]);										
-											color_list.splice(0, 1);
-										}
-									}
-								});
-								
-								// .on("mouseover", function(d) {
-								// 	div_tooltip.transition()
-								// 		.duration(200)
-								// 		.style("opacity", .9);
-								// 	div_tooltip
-								// 		.html("Dominance: " + d.weight)
-								// 		.style("left", d.x + "px")
-								// 		.style("top", d.y + "px");
-							 //   })
-								// .on("mouseout", function(d) {
-								// 	div_tooltip.transition()
-								// 		.duration(500)
-								// 		.style("opacity", 0);
-								// })
-								// .on("click", (d) => {
-								// 	if (d3.select("#pattern_" + d.id).classed("selected")) {
-								// 		_self.props.onMouseOutPattern(d.id);
-								// 		d3.select("#pattern_" + d.id).classed("selected", false);																				
-								// 	} else {
-								// 		_self.props.onMouseOverPattern(d.id);
-								// 		d3.select("#pattern_" + d.id).classed("selected", true);
-								// 	}
-								// });
-
+		// 12. Appends a circle for each datapoint 
+		d3.select(this.svg).selectAll(".dot")
+		    .data(dataset)
+		  .enter().append("circle") // Uses the enter().append() method
+		    .attr("class", "dot") // Assign a class for styling
+		    .attr("cx", function(d, i) { return xScale(i) })
+		    .attr("cy", function(d) { return yScale(d.y) })
+		    .attr("r", 5);
 
 
 
 
 
 	  return (
-			<div className={styles.PatternOverview}>
-				<div className={index.title}>Overview</div>
+			<div className={styles.ControlView}>
+				<div className={index.title}>ControlView</div>
 				{this.svg.toReact()}
-				<div className={styles.tooltop}></div>				
 			</div>
 	  );
 
 	}
 }
 
-export default OverView;
+export default ControlView;
