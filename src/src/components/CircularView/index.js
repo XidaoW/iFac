d5700f6baabac414aae3988a178397cf1cce80c1
xidaoW <mwen8103@gmail.com>
2019-02-15 15:57:24 -0500

    add force layout to jitter

--- src/src/components/CircularView/index.js
+++ src/src/components/CircularView/index.js
@@ -16,10 +16,6 @@ import QueryPanel from 'components/QueryPanel';
 
 const tooltip = d3tooltip(d3);
 
-/* props: this.props.ranking
-  => selected ranking data
-*/
-
 class CircularView extends Component {
   
 	constructor(props) {
@@ -74,13 +70,12 @@ class CircularView extends Component {
 		d3.selectAll('.query_bar').classed('queried', false)	
 		d3.selectAll('.query_bar').attr("stroke", "none");
 		d3.selectAll('.itemTags').remove()
-		this.props.onResetItems();
+		this.props.onResetItems();		
 	}
 
 	render() {
 		console.log('circularView rendered');
 		console.log('this.props.data: ', this.props.data);
-
 		const { data, selectedPatterns,
 				mostSimilarPatternToSelectedPatternIdx,
 				leastSimilarPatternToSelectedPatternIdx, 
@@ -88,20 +83,25 @@ class CircularView extends Component {
 				bar_data, max_pattern_item,modes,
 				queries, similarPatternToQueries, item_links, descriptors,
 				mouseOveredDescriptorIdx, item_similarity, components_cnt,
-				itemEmbeddings } = this.props;  
+				itemEmbeddings,patternEmbeddings } = this.props;  
 
 		const ButtonGroup = Button.Group;
+
+		var patternEmbedding_original = patternEmbeddings['tsne'];
+		var min_tsne = [d3.min(patternEmbedding_original, (d) => d[0]), d3.min(patternEmbedding_original, (d) => d[1])];
+		var max_tsne = [d3.max(patternEmbedding_original, (d) => d[0]), d3.max(patternEmbedding_original, (d) => d[1])];
 		const _self = this,
 					width = +this.layout.svg.width,
 					height = +this.layout.svg.height,
 					outerRadius = Math.min(width, height) - 20,
 					innerRadius = this.circularInnerRadius,
-					max_tsne = data[0].max_tsne,
-					min_tsne = data[0].min_tsne,
 					barFillOpacityConst = 0.5, 
 					query_flag = (Object.keys(queries).length == 0)? false: Object.keys(queries).map(function(key){			
 						return queries[key].length;
 					}).reduce((a,b)=>a+b);			
+
+		// max_tsne = data[0].max_tsne,
+		// min_tsne = data[0].min_tsne;
 
 		let	descriptor_size = Object.keys(bar_data).length,
 			descriptor_size_list = Object.keys(bar_data).map((d) => Object.keys(bar_data[d][0]).length),
@@ -116,7 +116,6 @@ class CircularView extends Component {
 
 		let g,
 			svg = new ReactFauxDOM.Element('svg');
-		console.log(bar_data);
 		svg.setAttribute('width', width+200);
 		svg.setAttribute('height',height+300);
 		svg.setAttribute('transform', 'translate(' + translate_x + ',' + translate_y + ')');
@@ -124,14 +123,30 @@ class CircularView extends Component {
 		this.pie = d3.pie().sort(null).value((d) => 1);
 		this.circle_color = d3.scaleLinear().domain([0, 1]).range(['#bf5b17','#e31a1c']).interpolate(d3.interpolateHcl);
 		this.circle_width = d3.scaleLinear().domain([0, 1]).range([1,2]);
-		this.circle_position_x = d3.scaleLinear().domain([min_tsne[0],max_tsne[0]]).range([- 0, + innerRadius]);
-		this.circle_position_y = d3.scaleLinear().domain([min_tsne[1],max_tsne[1]]).range([- 0, + innerRadius]);
+		this.circle_position_x = d3.scaleLinear().domain([min_tsne[0],max_tsne[0]]).range([- 0 + this.innerCircleRadius*2, + innerRadius - this.innerCircleRadius*2]);
+		this.circle_position_y = d3.scaleLinear().domain([min_tsne[1],max_tsne[1]]).range([- 0 + this.innerCircleRadius*2, + innerRadius - this.innerCircleRadius*2]);
 
 		// Update the list of available colors to pick for clicking patterns
 		for(var i = 0; i < selectedPatterns.length; i++){
 			used_color = d3.select('#pattern_' + selectedPatterns[i]).attr('stroke');   
 			color_list.splice( color_list.indexOf(used_color), 1 );
 		}
+
+		data.forEach((d, i) => {			
+				d.x = _self.circle_position_x(patternEmbedding_original[i][0]);
+				d.y = _self.circle_position_y(patternEmbedding_original[i][1]);
+				d.radius = parseInt(gs.innerCircleRadius);
+			}
+		);
+		// console.log(data);				
+		var simulation = d3.forceSimulation(data)
+			.force("x", d3.forceX(function(d) { return d.x; }).strength(0.1))
+			.force("y", d3.forceY(function(d) { return d.y; }).strength(0.1))
+			.force("collide", d3.forceCollide().radius(function(d){ return d.radius }))
+			.force("manyBody", d3.forceManyBody().strength(-10))
+			.stop();
+  		for (var i = 0; i < 50; ++i) simulation.tick();
+			// .force("center", d3.forceCenter((width)/2-(innerRadius)/2, (height)/2-( innerRadius)/2))
 
 		// draw the backdrop
 		const backdrop = d3.select(svg)
@@ -153,14 +168,14 @@ class CircularView extends Component {
 						.enter()
 						.append('circle')
 						.attr('class', 'pattern_circles')
-						.attr('r', gs.innerCircleRadius)
+						.attr('r', (d) => d.radius)
 						.attr('fill', '#fc8d12')
 						.attr('stroke-width', gs.innerCircleStrokeWidth)                
 						.attr('fill-opacity', (d) => d.weight) 
 						.attr('stroke-opacity', gs.innerCircleStrokeOpacity)
 						.attr('id', (d) => 'pattern_' + d.id)                
-						.attr('transform', (d, i) => 'translate(' + _self.circle_position_x(d.tsne_coord.x) + ',' 
-									+ _self.circle_position_y(d.tsne_coord.y) + ')')
+						.attr('transform', (d, i) => 'translate(' + d.x + ',' 
+									+ d.y + ')')
 						.on("mouseover", function(d){
 							tooltip.html('<div>pattern#' + d.id + '</div>'+ 
 								'<div>dominance: ' + d3.format(".0%")(d.weight) + '</div>');
@@ -211,14 +226,11 @@ class CircularView extends Component {
 									d3.select('#pattern_' + d.id).classed('selected', true);							
 									d3.select('#pattern_' + d.id).attr('stroke', color_list[0]);  
 									d3.select('circle#pattern_mini_' + d.id).attr("stroke", color_list[0]); 					
-
 									color_list.splice(0, 1);
-									this.props.onAddingPattern(d.id);
 								}
 							}
 							
 						});
-
 		// plot the flowers
 		const flowers = gFlowers.selectAll('.flower')
 						.data(data)
@@ -226,8 +238,9 @@ class CircularView extends Component {
 						.append('g')
 						.attr('class', 'flower')
 						.attr('id', (d) => 'flower_' + d.id)
-						.attr('transform', (d, i) => 'translate(' + _self.circle_position_x(d.tsne_coord.x) + ',' 
-									+ _self.circle_position_y(d.tsne_coord.y) + ')');
+						.attr('transform', (d, i) => 'translate(' +d.x + ',' 
+									+ d.y + ')');
+
 		// add the petals to the flowers
 		const petals = flowers.selectAll('.petal')
 					.data((d) => this.pie(d.petals))
@@ -299,8 +312,8 @@ class CircularView extends Component {
 						.data(similarPatternToQueries)
 						.enter()
 						.append('text')
-						.attr('transform', (d, i) => 'translate(' + (_self.circle_position_x(d.tsne_coord.x)-5) + ',' 
-									+ (_self.circle_position_y(d.tsne_coord.y)+5) + ')')
+						.attr('transform', (d, i) => 'translate(' + (data[d.pattern_idx].x- 3) + ',' 
+									+ (data[d.pattern_idx].y+3) + ')')
 	                	.text((d) => (d.rank+1).toString());
 			}else{
 				d3.select(".rankText").remove();
@@ -592,12 +605,43 @@ class CircularView extends Component {
 		};
 
 		function computeDescriptorRange(descriptor_index, descriptor_size,descriptor_size_list, shift_size){
+			/**
+			 * Compute the start angle and end angle for descriptor
+			 * 2 solutions 
+			 * a.1) compute the total size of a descriptor divided by descriptor count
+			 * a.2) obtain the maximum size of descriptor
+			 * a.3) compute each item size by dividing 1) by 2)
+			 * 
+			 * b.1) compute the unit size by dividing total item cnt
+			 * b.2) obtain the maximum size of descriptor
+			 * b.3) compute each item size by dividing 1) by 2)
+			 *
+			 * @since      0.0.0
+			 *
+			 *
+			 * @param {array}   similarPatternToQueries           a ranked list of similarity between pattern and query.
+			 * @param {boolean}  query_flag     if there is query input.
+			 * 
+			 */		
+
+			// solution a
 			var total_size = 2*Math.PI/descriptor_size,
 				max_item_cnt = d3.max(descriptor_size_list),
 				each_value = total_size*1. / max_item_cnt,
 				total_value_current = each_value * descriptor_size_list[descriptor_index],
 				start_value = 2*Math.PI*(descriptor_index+1)/descriptor_size + shift_size + (total_size - total_value_current) / 2.0,
 				end_value = 2*Math.PI*(descriptor_index+2)/descriptor_size - (total_size - total_value_current) / 2.0;
+
+			// solution b
+			// let new_array = [];			
+			// descriptor_size_list.reduce( (prev, curr,i) =>  new_array[i] = prev + curr , 0 );
+			// new_array = [0].concat(new_array);
+			// new_array = new_array.concat(new_array);
+			// var total_item_size = _.sum(descriptor_size_list),
+			// 	start_value,
+			// 	end_value,
+			// 	start_value = (2*Math.PI*new_array[descriptor_index])/total_item_size,
+			// 	end_value = (2*Math.PI*new_array[descriptor_index+1])/total_item_size;
 			return [start_value,  end_value];	
 		}  
 
@@ -633,30 +677,11 @@ class CircularView extends Component {
 						descriptors={descriptors}
 						components_cnt={components_cnt}
 						modes={modes}
-						queries={queries}
+						queries={this.props.queries}
 					/>
 				{svg.toReact()}				
 			</div>
 		);
   }
 }
-
-/* listview 2 
-class PatternGlyph extends Component {
-
-	render() {
-		return (
-			<div>
-				// svg that has all components for a glyph
-			</div>
-		);
-	}
-}
-
-export default {
-	CircularView, 
-	PatternGlyph
-}
-*/
-
 export default CircularView;
